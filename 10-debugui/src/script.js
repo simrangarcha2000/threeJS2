20b101ddffb0f781bf490cbb6badc2d88f1c4925
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2023-02-27 19:58:00 -0500

    Group Added

--- 10-debugui/src/script.js
+++ 10-debugui/src/script.js
@@ -24,9 +24,32 @@ const material = new THREE.MeshBasicMaterial(
     }
 );
 
-
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial({
+    color: "red"
+}))
+
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial({
+    color: "red"
+}))
+
+cube1.position.y = 2
+cube2.position.y = -2
+scene.add(cube1,cube2)
+
+
+//**                             GROUP                                         */
+
+const group = new THREE.Group();
+scene.add(group)
+
+group.add(cube1,mesh,cube2)
+group.rotation.set(7,4,5)
+
+
 
 //*TODO:                        CAMERA
 
@@ -37,7 +60,8 @@ const sizes = {
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 2
+camera.position.z = 5
+
 scene.add(camera)
 
 
